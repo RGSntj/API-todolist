a6eadd9d970e9b70c13d134078380ce950283e4c
Rodrigo Gomes <mariadefatima071965@gmail.com>
2023-10-13 23:15:32 -0300

    feat: add route of task delete

--- src/main/java/br/com/rodrigo/todolist/task/TaskController.java
+++ src/main/java/br/com/rodrigo/todolist/task/TaskController.java
@@ -7,6 +7,7 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -70,5 +71,24 @@ public class TaskController {
     var taskUpdated = this.taskRepository.save(task);
 
     return ResponseEntity.ok().body(taskUpdated);
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity delete(HttpServletRequest request, @PathVariable UUID id){
+    var task = this.taskRepository.findById(id).orElse(null);
+
+    if (task == null) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Tarefa não encontrada.");
+    }
+
+    var idUser = request.getAttribute("idUser");
+
+    if (!task.getIdUser().equals(idUser)) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Essa tarefa não pertence a você, Impossivel deletar.");
+    }
+
+    this.taskRepository.delete(task);
+
+    return ResponseEntity.ok().body("Task deletada com sucesso");
   }
 }
